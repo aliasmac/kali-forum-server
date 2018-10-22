8dade58eadcb06b8d3e9dbceef1b587123c0e6cc
thexyman <makdaa1@gmail.com>
2018-10-22 16:49:50 +0100

    all routes working

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,57 @@
+class Api::V1::CommentsController < ApplicationController
+  
+  def index 
+    @comments = Comment.all 
+    render json: @comments
+  end 
+
+  def new
+    @comment = Comment.new 
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save 
+      render json: @comment
+    else
+      render json: {error: "comment could not be created!"}, status: 400
+    end
+  end
+
+  def show 
+    @comment = Comment.find_by(id: params[:id])
+    if @comment
+      render json: @comment
+    else 
+      render json: {error: "comment not found"}, status: 404
+    end
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    if @comment.update(params)
+      render json: @comment 
+    else 
+      render json: {error: "comment not found"}, status: 404
+    end 
+  end
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+    render json: @comment
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy 
+    render json: @comments 
+  end
+
+  private 
+
+  def comment_params 
+    params.require(:comment).permit(:name)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'index/show'
-  get 'index/update'
-  get 'index/edit'
-  get 'index/destroy'
-  get 'index/create'
-  get 'index/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do 
-      resources :posts, :users  
+      resources :posts, :users, :comments   
     end 
   end 
 
